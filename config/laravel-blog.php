d266b4deb0db689e3aa559ba34f9265d41e1d7c9
Tom Lynch <tom@lnch.co.uk>
2018-04-30 15:42:50 +0100

    Add config file

---
+++ config/laravel-blog.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+      |--------------------------------------------------------------------------
+      | Blog Tag Options
+      |--------------------------------------------------------------------------
+      |
+      | All config options related to the blog tags are found in this section.
+      |
+      */
+
+    'tags' => [
+
+        'enabled'           => true,
+        'per-page'          => 15,
+
+    ]
+
+];
