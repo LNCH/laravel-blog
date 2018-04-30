3b5f054a8be2b89314a441ba61eb3bad83732a16
Tom Lynch <tom@lnch.co.uk>
2018-04-30 17:13:16 +0100

    Updated config file

--- config/laravel-blog.php
+++ config/laravel-blog.php
@@ -3,18 +3,58 @@
 return [
 
     /*
-      |--------------------------------------------------------------------------
-      | Blog Tag Options
-      |--------------------------------------------------------------------------
-      |
-      | All config options related to the blog tags are found in this section.
-      |
-      */
+    |--------------------------------------------------------------------------
+    | Route Prefix
+    |--------------------------------------------------------------------------
+    |
+    | By default, routes all routes start with 'blog'. If you want to customise
+    | this default routing, change the setting below to match the route prefix
+    | you want to use
+    |
+    */
+
+    'route_prefix' => 'blog',
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Path
+    |--------------------------------------------------------------------------
+    |
+    | The path the views can be found at. You should define this as you would
+    | when loading a view in a controller. i.e. "admin.blog"
+    |
+    */
+
+    'views_path' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Site Model
+    |--------------------------------------------------------------------------
+    |
+    | If you wish to use the blog across multiple 'sites', you will need to
+    | define a site model that implements the SiteInterface class provided in
+    | the package. You must provide the qualified class name.
+    |
+    */
+
+    'site_model' => Lnch\LaravelBlog\Site::class,
+
+    'site_primary_key' => 'id',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tag Options
+    |--------------------------------------------------------------------------
+    |
+    | All config options related to the blog tags are found in this section.
+    |
+    */
 
     'tags' => [
 
         'enabled'           => true,
-        'per-page'          => 15,
+        'per_page'          => 15,
 
     ]
 
