39f2f4ee688f4583f4aa9b7d170daae89cc3691d
Tom Lynch <tom@lnch.co.uk>
2018-04-30 17:14:06 +0100

    Add a base BlogModel that will scope the site if it is set, and also provide a site relation again if it is set

--- src/BlogModel.php
+++ src/BlogModel.php
@@ -2,17 +2,37 @@
 
 namespace Lnch\LaravelBlog;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogModel extends Model
 {
-    public function scopeSite($site)
+    protected static function boot()
     {
-        // Allow results to be filtered by site if applicable
+        parent::boot();
+
+        // Get site Model
+        $siteClass = config("laravel-blog.site_model");
+
+        if ($siteClass) {
+            $siteClassInstance = new $siteClass();
+
+            // Get site ID
+            $siteId = $siteClassInstance::getSiteId();
+
+            static::addGlobalScope('site', function(Builder $builder) use($siteId) {
+                $builder->where('site_id', $siteId);
+            });
+        }
     }
 
-    public function getCurrentSite()
+    public function site()
     {
-        // Find current site if applicable
+        if (config("laravel-blog.site_model")) {
+            return $this->belongsTo(config("laravel-blog.site_model"),
+                config("laravel-blog.site_primary_key"), "site_id");
+        }
+
+        return null;
     }
 }
