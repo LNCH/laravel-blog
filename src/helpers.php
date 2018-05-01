2f78f66705c7169137ff9c09b2e3b1c88c2ced15
Tom Lynch <tom@lnch.co.uk>
2018-05-01 13:01:13 +0100

    Add blogUrl() function

--- src/helpers.php
+++ src/helpers.php
@@ -10,3 +10,13 @@ function getBlogSiteID()
 
     return $siteClassInstance ? $siteClassInstance::getSiteId() : null;
 }
+
+function blogUrl($url)
+{
+    $routePrefix = config("laravel-blog.route_prefix", "");
+    if ($routePrefix && substr($routePrefix, -1) !== "/") {
+        $routePrefix .= "/";
+    }
+
+    return url($routePrefix.$url);
+}
