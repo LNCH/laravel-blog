9d584b899bfca51c094bf4ffd1948e7db8beba1c
Tom Lynch <tom@lnch.co.uk>
2018-05-01 17:13:19 +0100

    Include styles and Bootstrap JS

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -4,6 +4,8 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+        <link rel="stylesheet" href="{{ asset("vendor/lnch/laravel-blog/css/styles.css") }}" />
+
     </head>
 
     <body>
@@ -54,7 +56,14 @@
             @yield("content")
         </div>
 
-        <script src="{{ asset("vendor/lnch/laravel-blog/blog.js") }}"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+                crossorigin="anonymous"></script>
+
+        <script src="{{ asset("vendor/lnch/laravel-blog/js/blog.js") }}"></script>
 
     </body>
 </html>
