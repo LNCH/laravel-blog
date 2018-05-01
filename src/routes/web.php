1dfbf98ec4d3dc5ae61783ba98fcf0c9227f9ac5
Tom Lynch <tom@lnch.co.uk>
2018-05-01 13:00:52 +0100

    Add config checks to the routes file

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,9 +3,17 @@
 
 Route::group(['prefix' => config("laravel-blog.route_prefix"), 'middleware' => 'web'], function() {
 
-    Route::resource("tags", "Lnch\LaravelBlog\Controllers\BlogTagController", ['except' => [
-        'create', 'show'
-    ]]);
+    if (config("laravel-blog.tags.enabled")) {
+        Route::resource("tags", "Lnch\LaravelBlog\Controllers\BlogTagController", ['except' => [
+            'create', 'show'
+        ]]);
+    }
+
+    if (config("laravel-blog.categories.enabled")) {
+        Route::resource("categories", "Lnch\LaravelBlog\Controllers\BlogCategoryController", ['except' => [
+            'show'
+        ]]);
+    }
 
 });
 
