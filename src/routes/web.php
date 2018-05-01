15b90959256064420113e7238f88cf9f490e7e4f
Tom Lynch <tom@lnch.co.uk>
2018-05-01 17:13:03 +0100

    Code formatting

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,21 +4,30 @@
 Route::group(['prefix' => config("laravel-blog.route_prefix"), 'middleware' => 'web'], function() {
 
     if (config("laravel-blog.tags.enabled")) {
-        Route::resource(config("laravel-blog.tags.taxonomy"), "Lnch\LaravelBlog\Controllers\BlogTagController", ['except' => [
-            'create', 'show'
-        ]]);
+        Route::resource(config("laravel-blog.tags.taxonomy"),
+            "Lnch\LaravelBlog\Controllers\BlogTagController",
+            [
+                'except' => ['create', 'show']
+            ]
+        );
     }
 
     if (config("laravel-blog.categories.enabled")) {
-        Route::resource(config("laravel-blog.categories.taxonomy"), "Lnch\LaravelBlog\Controllers\BlogCategoryController", ['except' => [
-            'show'
-        ]]);
+        Route::resource(config("laravel-blog.categories.taxonomy"),
+            "Lnch\LaravelBlog\Controllers\BlogCategoryController",
+            [
+                'except' => ['show']
+            ]
+        );
     }
 
     if (config("laravel-blog.images.enabled")) {
-        Route::resource(config("laravel-blog.images.taxonomy"), "Lnch\LaravelBlog\Controllers\BlogImageController", ['except' => [
-            'show'
-        ]]);
+        Route::resource(config("laravel-blog.images.taxonomy"),
+            "Lnch\LaravelBlog\Controllers\BlogImageController",
+            [
+                'except' => ['show']
+            ]
+        );
     }
 
 });
