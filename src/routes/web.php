7807bd78e0d9763f47149406839f60e8c94ec4a1
Tom Lynch <tom@lnch.co.uk>
2018-05-04 16:11:50 +0100

    Typos and formating

--- src/routes/web.php
+++ src/routes/web.php
@@ -28,8 +28,8 @@ Route::group(['prefix' => config("laravel-blog.route_prefix"), 'middleware' => '
         );
     }
 
-    if (config("laravel-blog.images.enabled")) {
-
+    if (config("laravel-blog.images.enabled"))
+    {
         Route::post(config("laravel-blog.images.taxonomy")."/dialog-upload",
             "Lnch\LaravelBlog\Controllers\BlogImageController@dialogUpload");
         
