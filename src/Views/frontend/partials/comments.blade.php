aab6f3e291bc752bc371d8c0a7bc17ea7ca43e07
Tom Lynch <tom@lnch.co.uk>
2019-02-12 12:00:58 +0000

    Namespace comments list

--- src/Views/frontend/partials/comments.blade.php
+++ src/Views/frontend/partials/comments.blade.php
@@ -1,17 +1,19 @@
-<div class="comments">
+<ul class="lnch-blog_comments">
     @foreach($post->comments as $comment)
-        <div class="lnch-blog_post-comment">
-            <div class="comment-meta">
-                Posted by
-                <span class="user">
+        <li>
+            <div class="lnch-blog_post-comment">
+                <div class="comment-meta">
+                    Posted by
+                    <span class="user">
                 @if($comment->user) {{ $comment->user->name }} @else {{ $comment->name }} @endif
             </span>
-                on
-                <span class="date">{{ $comment->created_at->format("jS F, Y") }} </span>
-                at <span class="time">{{ $comment->created_at->format("h:ia") }}</span>
-            </div>
+                    on
+                    <span class="date">{{ $comment->created_at->format("jS F, Y") }} </span>
+                    at <span class="time">{{ $comment->created_at->format("h:ia") }}</span>
+                </div>
 
-            {{ $comment->body }}
-        </div>
+                {{ $comment->body }}
+            </div>
+        </li>
     @endforeach
-</div> <!-- End .comments -->
+</ul> <!-- End .comments -->
