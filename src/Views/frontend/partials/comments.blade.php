078bd2467f30fb7e7ddc497cf276aa514b74a64e
Tom Lynch <tom@lnch.co.uk>
2019-02-12 11:52:31 +0000

    Add structure to comment post

--- src/Views/frontend/partials/comments.blade.php
+++ src/Views/frontend/partials/comments.blade.php
@@ -1,9 +1,15 @@
 @foreach($post->comments as $comment)
     <div class="lnch-blog_post-comment">
-        Posted by
-        @if($comment->user) {{ $comment->user->name }} @else {{ $comment->name }} @endif
-        on {{ $comment->created_at->format("jS F, Y") }} at {{ $comment->created_at->format("h:ia") }}
-        <br /><br />
+        <div class="comment-meta">
+            Posted by
+            <span class="user">
+                @if($comment->user) {{ $comment->user->name }} @else {{ $comment->name }} @endif
+            </span>
+            on
+            <span class="date">{{ $comment->created_at->format("jS F, Y") }} </span>
+            at <span class="time">{{ $comment->created_at->format("h:ia") }}</span>
+        </div>
+
         {{ $comment->body }}
     </div>
 @endforeach
