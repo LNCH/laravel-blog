50897bfbe635d923851790fdcb7829685de91a77
Tom Lynch <tom@lnch.co.uk>
2019-02-12 11:56:57 +0000

    Wrap comments in div

--- src/Views/frontend/partials/comments.blade.php
+++ src/Views/frontend/partials/comments.blade.php
@@ -1,15 +1,17 @@
-@foreach($post->comments as $comment)
-    <div class="lnch-blog_post-comment">
-        <div class="comment-meta">
-            Posted by
-            <span class="user">
+<div class="comments">
+    @foreach($post->comments as $comment)
+        <div class="lnch-blog_post-comment">
+            <div class="comment-meta">
+                Posted by
+                <span class="user">
                 @if($comment->user) {{ $comment->user->name }} @else {{ $comment->name }} @endif
             </span>
-            on
-            <span class="date">{{ $comment->created_at->format("jS F, Y") }} </span>
-            at <span class="time">{{ $comment->created_at->format("h:ia") }}</span>
-        </div>
+                on
+                <span class="date">{{ $comment->created_at->format("jS F, Y") }} </span>
+                at <span class="time">{{ $comment->created_at->format("h:ia") }}</span>
+            </div>
 
-        {{ $comment->body }}
-    </div>
-@endforeach
+            {{ $comment->body }}
+        </div>
+    @endforeach
+</div> <!-- End .comments -->
