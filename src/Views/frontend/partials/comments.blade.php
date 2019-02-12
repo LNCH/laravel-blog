0a9b12fa3924a9618e8213559d4af41d2b196ee4
Tom Lynch <tom@lnch.co.uk>
2019-02-12 09:47:08 +0000

    Remove inline styles

--- src/Views/frontend/partials/comments.blade.php
+++ src/Views/frontend/partials/comments.blade.php
@@ -1,5 +1,5 @@
 @foreach($post->comments as $comment)
-    <div style="padding: 1rem; border: 1px solid grey; margin-bottom: 1rem;">
+    <div class="lnch-blog_post-comment">
         Posted by
         @if($comment->user) {{ $comment->user->name }} @else {{ $comment->name }} @endif
         on {{ $comment->created_at->format("jS F, Y") }} at {{ $comment->created_at->format("h:ia") }}
